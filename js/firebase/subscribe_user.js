9b848f2779379895f5a3618b76c833bf14b6a1f6
getulio.souza <getulio.souza@evolutix.com.br>
2022-10-23 13:17:08 -0300

    removed-previous-javascript

--- js/firebase/subscribe_user.js
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import { getFirestore, collection, addDoc} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js'
-
-export async function subscribeUser(subscription) {
-    const db = getFirestore(app)
-    const travellerUserCollection = collection(db,"usuario_traveller")
-    const docRef = await addDoc(travellerUserCollection, subscription)
-    return docRef.id
-}

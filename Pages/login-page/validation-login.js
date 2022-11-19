f87e233d69f53a284f8b35b60a3d9601a30343c7
getulio.souza <getulio.souza@evolutix.com.br>
2022-11-19 17:11:59 -0300

    refactoring-validation-login-js

---
+++ Pages/login-page/validation-login.js
@@ -0,0 +1,60 @@
+function onChangeEmail() {
+    toogleButtonsDisable();
+    toogleEmailErrors();
+}
+
+function onChangePassword() {
+    toogleButtonsDisable();
+    tooglePasswordErrors();
+}
+
+//check to know is e-email is valid
+function isEmailValid() {
+    const email = form.email().value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+//show or hide error e-mail error
+function toogleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+}
+
+function tooglePasswordErrors() {
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+
+}
+
+
+//buttons disable
+function toogleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+}
+
+//validate password
+function isPasswordValid() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+const form = {
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    loginButton: () => document.getElementById('login-button'),
+    password: () => document.getElementById('password'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    recoverPassword: () => document.getElementById('recover-password-button')
+}
---
+++ assets/js/validations.js
@@ -0,0 +1,4 @@
+//validate email
+function validateEmail(email) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+}

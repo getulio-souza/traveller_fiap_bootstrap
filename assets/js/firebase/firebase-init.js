f186cd076025f6c685f5fdcf5e5b80c1d07b6f7d
getulio.souza <getulio.souza@evolutix.com.br>
2022-11-21 09:33:09 -0300

    showing-error-if-user-already-exists

---
+++ assets/js/firebase/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxZOrfIYjQ0iLaEZkQh6brUVvuuprkvWQ",
+    authDomain: "authentication-gulliver.firebaseapp.com",
+    projectId: "authentication-gulliver",
+    storageBucket: "authentication-gulliver.appspot.com",
+    messagingSenderId: "45448383265",
+    appId: "1:45448383265:web:8553e4691e29d13b3ecc60"
+};
+firebase.initializeApp(firebaseConfig);
